135aa649eba57850b84151258c727c5c2c7b4272
Fernando Consalvo <fconsalvo97@gmail.com>
2020-01-27 13:43:53 -0400

    Add assets and fix metisMenu for sidebar

--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import 'metismenu';
+// tslint:disable-next-line:no-any
+declare let $: any;
+
 
 @Component({
-  selector: 'app-side-bar',
-  templateUrl: './side-bar.component.html',
-  styleUrls: ['./side-bar.component.css']
+    selector: 'app-side-bar',
+    templateUrl: './side-bar.component.html',
+    styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent implements OnInit {
 
-  constructor() { }
+    public ariaExpanded = false;
 
-  ngOnInit() {
-  }
+    constructor() {
+
+    }
+
+    ngOnInit() {
+        $('#side-menu').metisMenu();
+    }
+
+    changeAriaExpanded() {
+        this.ariaExpanded = !this.ariaExpanded;
+    }
 
 }
