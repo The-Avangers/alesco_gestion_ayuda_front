96ca561f998c3dd4ec9bd458b1fd911b04fcb862
Fernando Consalvo <fconsalvo97@gmail.com>
2020-02-10 13:38:21 -0400

    Add project form template

--- src/app/project-form/project-form.component.ts
+++ src/app/project-form/project-form.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {Institution} from '../services/institution/institution.interface';
 import {InstitutionService} from '../services/institution/institution.service';
 import {ProjectsService} from '../services/project/projects.service';
 import {Select2OptionData} from 'ng2-select2';
@@ -11,12 +10,19 @@ import {Select2OptionData} from 'ng2-select2';
 })
 export class ProjectFormComponent implements OnInit {
 
+    selectOptions: Select2Options;
     institutions: Select2OptionData[];
+    private now = new Date();
 
     constructor(private projectsService: ProjectsService, private institutionsService: InstitutionService) {
     }
 
     ngOnInit() {
+        this.selectOptions = {
+            matcher: (term: string, text: string) => {
+                return text.toUpperCase().indexOf(term.toUpperCase()) === 0;
+            }
+        };
         this.institutionsService.getInstitutions().subscribe(response => {
             console.log(response);
             this.institutions = response.map(value => {
@@ -25,10 +31,16 @@ export class ProjectFormComponent implements OnInit {
                     text: null
                 };
                 select2.id = value.id.toString();
-                select2.text = value.name;
+                select2.text = `(${value.id}) ${value.name}`;
                 return select2;
             });
         });
+    }
+
+    getMinEndDate() {
+        const month = (this.now.getMonth() + 1) >= 10 ? this.now.getMonth() + 1 : `0${this.now.getMonth() + 1}`;
+        console.log(`${this.now.getFullYear()}-${month}-${this.now.getDate()}`);
+        return `${this.now.getFullYear()}-${month}-${this.now.getDate()}`;
     }
 
 }
