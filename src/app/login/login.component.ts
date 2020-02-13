5dfcf87e5c16a68345cd637ac8ff45e1fb193650
gabrielbaron16 <gabrielbu161@gmail.com>
2020-02-12 21:34:24 -0400

    Añadiendo Form de Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  submitted = false;
+  LoginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+  });
   constructor() { }
-
   ngOnInit() {
+  }
+    get f() { return this.LoginForm.controls; }
+  login() {
+      this.submitted = true;
+      if (this.LoginForm.invalid) {
+          console.log('negado')
+          return;
+      }
   }
 
 }
