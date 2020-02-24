70b62af95f76e86dddb03c5a45c0dd70104ec885
Fernando Consalvo <fconsalvo97@gmail.com>
2020-02-23 21:03:01 -0400

    Fix spinner in project form

--- src/app/project-form/project-form.component.ts
+++ src/app/project-form/project-form.component.ts
@@ -103,9 +103,9 @@ export class ProjectFormComponent implements OnInit {
                         this.f.peopleInCharge.setValue(this.peopleInChargeCurrent.map(value => value.toString()));
                         this.f.institution.setValue(this.institutions.find(value => value.text === response.institution).id);
                         this.f.price.setValue(response.price);
-                        this.isLoading = false;
                     });
                 }
+                this.isLoading = false;
             });
         });
 
